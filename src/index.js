41767c9ad602baee7eb8496be91bb73801548649
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-25 22:28:34 +0530

    Customised electron settings

--- src/index.js
+++ src/index.js
@@ -10,18 +10,12 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        nodeIntegration: true,
-        contextIsolation: false,
-        enableRemoteModule: true,
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 700,
     });
 
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -29,22 +23,8 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
-app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
