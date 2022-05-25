42d9a50f5b426dd4e0161d0fa2515acb8405be5a
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-25 17:53:56 +0530

    Added field and logic for headers

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const decodebutton = document.getElementById('decodejwtbtn');
 const clearbutton = document.getElementById('clearjwtbtn');
 const input = document.getElementById('tokeninput');
 const result = document.getElementById('tokenresult');
+const headers = document.getElementById('tokenheaders');
 
+// TODO: Get rid of console logs and use better alerts
 decodebutton.addEventListener('click', function () {
 	// value works instead of innerHTML or innerText because this is a form element
 	let token = input.value;
@@ -13,10 +15,13 @@ decodebutton.addEventListener('click', function () {
 		try {
 			// eslint-disable-next-line no-undef
 			let decoded = jwt_decode(token);
-			let myJSON = JSON.stringify(decoded, null, 4);
-			console.log(myJSON);
-			result.textContent = myJSON;
+			let decodedJSON = JSON.stringify(decoded, null, 4);
+			result.textContent = decodedJSON;
 
+			// eslint-disable-next-line no-undef
+			var decodedHeader = jwt_decode(token, { header: true });
+			let decodedHeaderJSON = JSON.stringify(decodedHeader, null, 4);
+			headers.textContent = decodedHeaderJSON;
 		} catch (e) {
 			if (e.name === 'InvalidTokenError') {
 				console.log('The token is invalid!');
