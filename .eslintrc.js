4c31ac2c9790b9aea20e4b8db43dc7fe9b1f21a3
Ayush Rao <ayushvijayrao@gmail.com>
2022-05-25 17:58:54 +0530

    Updated eslint to use spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
--- index.js
+++ index.js
@@ -6,37 +6,37 @@ const headers = document.getElementById('tokenheaders');
 
 // TODO: Get rid of console logs and use better alerts
 decodebutton.addEventListener('click', function () {
-	// value works instead of innerHTML or innerText because this is a form element
-	let token = input.value;
-	// === takes into account data type whereas == does not
-	if (token === '') {
-		console.log('The field is empty');
-	} else {
-		try {
-			// eslint-disable-next-line no-undef
-			let decoded = jwt_decode(token);
-			let decodedJSON = JSON.stringify(decoded, null, 4);
-			result.textContent = decodedJSON;
+    // value works instead of innerHTML or innerText because this is a form element
+    let token = input.value;
+    // === takes into account data type whereas == does not
+    if (token === '') {
+        console.log('The field is empty');
+    } else {
+        try {
+            // eslint-disable-next-line no-undef
+            let decoded = jwt_decode(token);
+            let decodedJSON = JSON.stringify(decoded, null, 4);
+            result.textContent = decodedJSON;
 
-			// eslint-disable-next-line no-undef
-			var decodedHeader = jwt_decode(token, { header: true });
-			let decodedHeaderJSON = JSON.stringify(decodedHeader, null, 4);
-			headers.textContent = decodedHeaderJSON;
-		} catch (e) {
-			if (e.name === 'InvalidTokenError') {
-				console.log('The token is invalid!');
-				console.log('Error:', e);
-			}
-			else {
-				console.log('Another error has occured', e);
-				console.log('Error:', e);
-			}
-		}
-	}
+            // eslint-disable-next-line no-undef
+            var decodedHeader = jwt_decode(token, { header: true });
+            let decodedHeaderJSON = JSON.stringify(decodedHeader, null, 4);
+            headers.textContent = decodedHeaderJSON;
+        } catch (e) {
+            if (e.name === 'InvalidTokenError') {
+                console.log('The token is invalid!');
+                console.log('Error:', e);
+            }
+            else {
+                console.log('Another error has occured', e);
+                console.log('Error:', e);
+            }
+        }
+    }
 }
 );
 
 clearbutton.addEventListener('click', function () {
-	input.value = '';
-	result.textContent = 'Output goes here...';
+    input.value = '';
+    result.textContent = 'Output goes here...';
 });
